6ec8123ebab644905f1fe9ae75ce495245a7a556
Ari Mourao <amlima@gmail.com>
2019-11-16 20:05:20 -0300

    small bug fixes

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
 sudo apt-get snapd git vim neovim xclip wget curl -y
 
 #chrome
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb |
+wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
 #firefox dev edition
@@ -13,14 +13,14 @@ wget "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=lin
 sudo tar -jxvf  firefox-developer.tar.bz2 -C /opt/
 sudo mv /opt/firefox*/ /opt/firefox-developer
 sudo ln -sf /opt/firefox-developer/firefox /usr/bin/firefox-developer
-echo -e '[Desktop Entry]\n Version=59.0.3\n Encoding=UTF-8\n Name=Mozilla Firefox\n Comment=Navegador Web\n Exec=/opt/firefox-developer/firefox\n Icon=/opt/firefox-developer/browser/chrome/icons/default/default128.png\n Type=Application\n Categories=Network' | sudo tee /usr/share/applications/firefox-developer.desktop
+echo -e '[Desktop Entry]\n Version=59.0.3\n Encoding=UTF-8\n Name=Firefox Developer Edition\n Comment=Navegador Web\n Exec=/opt/firefox-developer/firefox\n Icon=/opt/firefox-developer/browser/chrome/icons/default/default128.png\n Type=Application\n Categories=Network' | sudo tee /usr/share/applications/firefox-developer.desktop
 
 #docker
 sudo apt-get install \
      apt-transport-https \
      ca-certificates \
      gnupg-agent \
-     software-properties-common
+     software-properties-common -y
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
@@ -29,19 +29,19 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 #docker-compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 #snapd packages
-sudo snap code --classic
-sudo snap emacs --classic
-sudo snap skype --classic
-sudo snap tusk
-sudo snap spotify
-sudo snap telegram-desktop
+sudo snap install code --classic
+sudo snap install emacs --classic
+sudo snap install skype --classic
+sudo snap install tusk
+sudo snap install spotify
+sudo snap install telegram-desktop
 
 #spacemacs
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
@@ -61,7 +61,7 @@ wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
 echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/dont-prompt-$USER-for-password
 
 #generate ssh key
-echo -e "\n\n" | ssh-keygen -t rsa -b 4096 -C "amlima@gmail.com"
+yes "\r" | ssh-keygen -t rsa -b 4096 -C "amlima@gmail.com"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
 
