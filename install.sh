8c721a4281018c2fbaa052990fa3f49b59030b96
Ari Mourao <amlima@gmail.com>
2024-05-01 09:30:06 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,72 +1,45 @@
 #!/bin/bash
 
-#apt packages
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get dist-upgrade -y
-sudo apt-get snapd git vim neovim xclip wget curl -y
+echo "Atualizando lista de pacotes e atualizando-os..."
+sudo apt-get update && sudo apt-get upgrade -y
 
-#chrome
-wget "https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
-sudo dpkg -i google-chrome-stable_current_amd64.deb
+echo "Instalando pacotes necessários para Docker, VirtualBox e outros..."
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
-#firefox dev edition
-wget "https://download.mozilla.org/?product=firefox-devedition-latest-ssl&os=linux64&lang=en-US" -O firefox-developer.tar.bz2
-sudo tar -jxvf  firefox-developer.tar.bz2 -C /opt/
-sudo mv /opt/firefox*/ /opt/firefox-developer
-sudo ln -sf /opt/firefox-developer/firefox /usr/bin/firefox-developer
-echo -e '[Desktop Entry]\n Version=59.0.3\n Encoding=UTF-8\n Name=Firefox Developer Edition\n Comment=Navegador Web\n Exec=/opt/firefox-developer/firefox\n Icon=/opt/firefox-developer/browser/chrome/icons/default/default128.png\n Type=Application\n Categories=Network' | sudo tee /usr/share/applications/firefox-developer.desktop
+echo "Adicionando a chave GPG do Docker..."
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
-#docker
-sudo apt-get install \
-     apt-transport-https \
-     ca-certificates \
-     gnupg-agent \
-     software-properties-common -y
+echo "Adicionando o repositório do Docker..."
+echo | sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+echo "Adicionando repositório do VirtualBox..."
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+echo | sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
 
-sudo add-apt-repository \
-     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+echo "Atualizando pacotes após adicionar novos repositórios..."
+sudo apt-get update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+echo "Instalando Docker Engine, Docker Compose e VirtualBox..."
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io virtualbox-6.1
 
-#docker-compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+echo "Permitindo o usuário $USER executar Docker sem sudo..."
+sudo usermod -aG docker $USER
+
+echo "Instalando Docker Compose..."
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-#snapd packages
-sudo snap install code --classic
-sudo snap install emacs --classic
-sudo snap install skype --classic
-sudo snap install tusk
-sudo snap install spotify
-sudo snap install telegram-desktop
+echo "Instalando e configurando Zsh e Oh My Zsh..."
+sudo apt-get install -y zsh
+chsh -s $(which zsh)
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-#spacemacs
-git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
+echo "Instalando e habilitando o servidor SSH..."
+sudo apt-get install -y openssh-server
+sudo systemctl enable ssh
+sudo systemctl start ssh
 
-#spacemacs fonts
-git clone --depth 1 --branch release https://github.com/adobe-fonts/source-code-pro.git ~/.fonts/adobe-fonts/source-code-pro
-fc-cache -f -v ~/.fonts/adobe-fonts/source-code-pro
+echo "Gerando chaves SSH para o usuário..."
+ssh-keygen -t rsa -b 4096 -f ~/.ssh/id_rsa -N ""
 
-#bash-it
-git clone --depth=1 https://github.com/arimourao/bash-it.git ~/.bash_it
-~/.bash_it/install.sh --silent
-
-#nvm
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.1/install.sh | bash
-
-#create sudo rules for my user
-echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/dont-prompt-$USER-for-password
-
-#generate ssh key
-yes "\r" | ssh-keygen -t rsa -b 4096 -C "email"
-eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/id_rsa
-
-#copy ssh pubkey to clipboard
-xclip -sel clip < ~/.ssh/id_rsa.pub
-
-source ~/.bashrc
-
+echo "Configuração completa!"
